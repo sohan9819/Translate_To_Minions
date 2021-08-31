49e7aa2c52c562a5126b1b9f4b318da6f90c37ca
sohan9819 <sohanshetty2001@gmail.com>
2021-08-31 19:15:04 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const inputArea = document.querySelector("#inputText");
+const translateBtn = document.querySelector('#submit');
+const outputArea = document.querySelector('#outputText');
+
+translateBtn.addEventListener('click' , function(e){
+    e.preventDefault();
+    let url = Url(inputArea.value);
+    if ( inputArea.value === ""){
+        outputArea.value = ""
+    }else {
+        translate(url);
+    }
+});
+
+const translate = (url) => {
+    fetch(url)
+    .then (res => {
+        return res.json()
+    })
+    .then(data => {
+        // console.log("Data Parsed");
+        outputArea.value = data.contents.translated;
+    })
+    .catch(err => {
+        console.log("Error!!!!" , err)
+    })
+}
+
+const Url = (text) => {
+    return 'https://api.funtranslations.com/translate/minion.json?text=' + `"${text}"`;
+}
+
+inputArea.addEventListener('click' , function(){
+    // console.dir(outputArea)
+    outputArea.value="";
+})
